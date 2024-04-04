713da7e9328a1df93c5c63434f65c6db7af13d8a
imshubham07 <mrshubhamdubey20@gmail.com>
2024-04-04 19:10:25 +0530

    My First Commit is React and some basic concept

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1>Code in React | Shubham Kumar Dubey|in this project mai ye sikha ki react ka basic project kaise banate ha</h1>
+    
+    //react folder normal build karne ke liye "npx create-react-app" command ka use karte
+  );
+}
+
+export default App;
