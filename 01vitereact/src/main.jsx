8b563238b46bfd99772e2bb39d8f35e116b060d2
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-05 00:39:23 +0530

    Create Own React and some point

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,46 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
 
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Custom App !</h1>
+//     </div>
+//   );
+// }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
- 
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const anotherUser = "__userName"
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href:'https://google.com', target:'_blank'
+  },
+  'click here to visit google',
+  anotherUser
 )
+
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">click me to visit google</a>
+)
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  
+   
+  reactElement 
+  // <App />
+    
+  
+);
