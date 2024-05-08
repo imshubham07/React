447c4fe1bee29f7ded9a7c9549a7a4a8bd1171eb
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-09 00:09:42 +0530

    Password Generator Making in react

--- passwordGenrator/src/App.jsx
+++ passwordGenrator/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useCallback } from "react";
 import "./App.css";
 
 function App() {
-  const [length, setLength] = useState(0);
+  const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [Password, setPassword] = useState("");
@@ -15,17 +15,43 @@ function App() {
     if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
 
     for (let i = 1; i <= array.length; i++) {
-      let char = Math.floor(Math.random()* str.length + 1)
-      pass = str.charAt(char)
-      
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
     }
-    setPassword(pass)
-
+    setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg my-8 text-orange-500 bg-gray-700"></div>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700 ">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className ="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={Password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password Generated"
+            readOnly
+          />
+          <button
+          className="outline-none bg-pink-500 text-white px-3 py-0.5 shrink-0">Copy</button>
+
+
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {setLength(e.target.value)}}
+            />
+            <lebel>Length:{length}</lebel>
+          </div>
+          
+        </div>
+      </div>
     </>
   );
 }
