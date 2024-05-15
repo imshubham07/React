b72c008859e442e916ba660e20c1c81a69f34fde
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-15 16:22:36 +0530

    React Routing

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-600 p-4'>React router</h1>
+      
     </>
   )
 }
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import Footer from './components/footer/Footer.jsx'
+import About from './components/About/About.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children:[
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
