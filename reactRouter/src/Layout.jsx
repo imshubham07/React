4d75bd5e91039aec259d119c159bb60d92cabfe0
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-15 23:43:59 +0530

    React Router

--- reactRouter/src/Layout.jsx
+++ reactRouter/src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 export function Layout(props) {
