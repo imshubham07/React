96e99cc4ad13412618c6e22c7dfd16fac79389a5
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-16 19:52:18 +0530

    React Router

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -6,6 +6,8 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact-us/Contact.jsx'
+import { User } from './components/User/User.jsx'
+import  GitHub, {githubInfoLoader} from './components/Github/GitHub.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -36,6 +38,12 @@ const router = createBrowserRouter(
       <Route path='' element = {<Home/>} />
       <Route path='about' element = {<About/>} />
       <Route path='contact' element = {<Contact/>} />
+      <Route path='user/:userid' element = {<User/>} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element = {<GitHub/>} 
+      />
     </Route>
   )
 )
