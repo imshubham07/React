e632203163fc7cc9181298d064ae8aeb44d37b1f
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-17 22:15:16 +0530

    Context API  in React

--- miniContextAPI/src/App.jsx
+++ miniContextAPI/src/App.jsx
@@ -1,12 +1,13 @@
+import UserContexProvider from "./context/UserContexProvider"
 
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <UserContexProvider>
+      <h1>Learning React and topic Name is miniContexAPI </h1>
+    </UserContexProvider>
   )
 }
 
---
+++ miniContextAPI/src/Component/LogIn.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useContext} from 'react'
+import UserContext from '../context/UserContex'
+
+export function LogIn() {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('') 
+
+    const {setUser} UserContext(UserContext)
+    const handleSubmit = () => {
+
+    }
+    return (
+       
+            <div>
+                <h2>Login</h2>
+                <input type="text" 
+                value={username} 
+                onChange = {(e) => setUsername(e.target.value)} placeholder='username'/>
+                <input type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='password'/>
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+       
+    )
+}
+export  default LogIn
