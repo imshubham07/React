3ef115131225459e6ee0d3bdb7c4fe76ce9388e7
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-19 11:49:44 +0530

    Card Component with theme changer

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./contexts/theme";
 import { useEffect } from "react";
+import ThemeBtn from './Components/Themebtn'
+import Card from './Components/Card'
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
@@ -27,11 +29,11 @@ function App() {
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* themebtn */}
+          <ThemeBtn/>
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-          {/* Card */}
+          <Card />
         </div>
       </div>
     </div>
