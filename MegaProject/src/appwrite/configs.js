6437e6f7a6545b76f5a3ee4e880ca3ea94c8f3a2
imshubham07 <mrshubhamdubey20@gmail.com>
2024-05-31 15:26:01 +0530

    Mega Project in React

--- MegaProject/src/appwrite/configs.js
+++ MegaProject/src/appwrite/configs.js
@@ -77,7 +77,7 @@ export class Service{
         }
     }
 
-    async getPosts(queries = [Query.equal("status", "active")]){
+    async getsPosts(queries = [Query.equal("status", "active")]){
         try {
             return await  this.databases.listDocuments(
                 config.appwriteDatabaseId,
