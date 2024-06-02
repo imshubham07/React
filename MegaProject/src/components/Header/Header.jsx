767bb1418caed027185ba2bb0e1765eb34e41de6
imshubham07 <mrshubhamdubey20@gmail.com>
2024-06-02 23:31:46 +0530

    Mega Project

--- MegaProject/src/components/Header/Header.jsx
+++ MegaProject/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import {Container, LogoutBtn, Logo} from "'../index.js"
+import {Container, LogoutBtn, Logo} from "../index.js"
 import { Link } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
